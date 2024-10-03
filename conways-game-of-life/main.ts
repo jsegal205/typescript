922d12e395bbb83edc28f7a3260e7919a097cce2
Jim Segal <hello@jimsegal.com>
2024-10-03 12:22:03 -0500

    cgol: clean up neighbor count checks

--- conways-game-of-life/main.ts
+++ conways-game-of-life/main.ts
@@ -1,59 +1,59 @@
 
+const alive = "*"
+const dead = ""
 
-const createGrid: (col: number, row: number) => boolean[][] = (col, row) => {
+const createGrid: (col: number, row: number) => string[][] = (col, row) => {
   if (!Boolean(col) || !Boolean(row)) {
     console.log("please supply a column value and row value")
   }
   // setup grid and init random set of cells
-  return new Array(row).fill(false).map(() => new Array(col).fill(false).map(() => Math.random() < 0.5))
-    // return new Array(row).fill(false).map(() => new Array(col).fill(false))
+  return new Array(row).fill(false).map(() => new Array(col).fill(false).map(() => Math.random() < 0.5 ? alive : dead))
 }
 
-const run: (grid: boolean[][], iterations: number) => void = (grid, iterations = 3) => {
+const getLiveNeighbors = (grid: string[][], rowIndex: number, colIndex: number ) => {
+  const directions: Array<[number, number]> = [
+    [-1,-1], [-1,0], [-1,1],
+    [0,-1],         [0,1],
+    [1,-1], [1,0], [1,1],
+  ]
+
+  return directions.reduce((liveCount, [rowOffset, colOffset]) => {
+    const rowToCheck = rowIndex + rowOffset
+    const colToCheck = colIndex + colOffset
+
+    if (rowToCheck >= 0 && rowToCheck < grid.length && colToCheck >= 0 && colToCheck < grid[rowIndex].length) {
+      if (grid[rowToCheck][colToCheck] === alive) {
+        liveCount++
+      }
+    }
+
+    return liveCount
+  }, 0)
+}
+
+const run: (grid: string[][], iterations: number) => void = (grid, iterations = 3) => {
   // run as many times as number of iterations
   // iterate over each cell
   // check biz rules, update cells accordingly
-  // for (let iteration = 0; iteration <= iterations; iteration++) {
-    console.log('iteration #', iterations)
+
     console.log(grid)
-    // grid
     const iterationGrid = grid.map((row, rowIndex) => {
       return row.map((col, colIndex) => {
-        // console.log("row, col, alive?", rowIndex, colIndex, col)
+        const countLiveNeighbors = getLiveNeighbors(grid, rowIndex, colIndex)
 
-        const hasAbove = rowIndex > 0
-        const hasLeft = colIndex > 0
-        const hasRight = colIndex + 1 !== row.length
-        const hasBelow = rowIndex + 1 !== grid.length
-
-        // figure out what to do with walls
-        const aboveLeft = hasAbove && hasLeft ? grid[rowIndex - 1][colIndex - 1] : "above left wall"
-        const above = hasAbove ? grid[rowIndex - 1][colIndex] : "above wall"
-        const aboveRight = hasAbove && hasRight ? grid[rowIndex - 1][colIndex + 1] : "above right wall"
-
-        const rowLeft = hasLeft ? grid[rowIndex][colIndex - 1] : "row left wall"
-        const rowRight = hasRight ? grid[rowIndex][colIndex + 1] : "row right wall"
-
-        const belowLeft = hasBelow && hasLeft ? grid[rowIndex + 1][colIndex - 1] : "below left wall"
-        const below = hasBelow  ? grid[rowIndex + 1][colIndex] : "below wall"
-        const belowRight = hasBelow && hasRight ? grid[rowIndex + 1][colIndex + 1] : "below right wall"
-
-        const countLiveNeighbors = [aboveLeft, above, aboveRight, rowLeft, rowRight, belowLeft, below, belowRight].filter((alive) => alive === true).length
-        // console.log(countLiveNeighbors)
-
-        if (col === true) {
-          //currently alive cell
+        if (col === alive) {
           if ([2,3].includes(countLiveNeighbors)) {
              // console.log("should survive")
+             return alive
           } else {
             // console.log("should die")
-            return false
+            return dead
           }
-        } else {
+        } else if (col === dead) {
           if (countLiveNeighbors === 3) {
             // console.log("should become alive")
-            return true
-          } else {
+            return alive
+            // } else {
             // console.log("stay dead :sad:")
           }
         }
@@ -65,13 +65,12 @@ const run: (grid: boolean[][], iterations: number) => void = (grid, iterations =
     if (iterations > 0) {
       run(iterationGrid, iterations - 1)
     }
-
-  // }
 }
 
 const gameOfLife: () => void = () => {
-  const grid = createGrid(3,3);
+  const grid = createGrid(10, 10);
   run(grid, 2)
+  console.log("complete")
 }
 
 // run the script
